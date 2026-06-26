f61634ab3c047322f64470454e7385ffd48848f8
veseling02 <veskog43@gmail.com>
2026-06-26 19:00:59 +0300

    add initial game setup with Pygame for telemetry display

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 import pygame
+import harvester
 
+pygame.init()
+
+WIDTH, HEIGHT = 600, 400
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Forza Live Telemetry")
+
+clock = pygame.time.Clock()
+
+x_step = WIDTH / 240
+
+running  = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
